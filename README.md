28349606ca4fab84e4eb31b1db27a219363232b1
Abhinav Mishra <makeawish880@gmail.com>
2019-03-31 15:30:39 +0700

    Deploy setup (#1)
    * Change package config of frontend app to deploy on Heroku
    
    * Add deploy scripts

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # angularattack2019-triangleflag
+
+## Deployment Instructions
+
+The app uses two servers for deployment, one for frontend & other for backend.
+Both apps can be deployed at once by running the script 
+
+```
+./devops/deploy.sh
+```
--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   hmr: false,
   title: 'Phangular',
-  version: require('../../package.json').version
+  version: require('../../package.json').version,
+  socketURL: 'https://triangleflag-api.herokuapp.com'
 };
