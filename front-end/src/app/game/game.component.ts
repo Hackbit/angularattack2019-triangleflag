df0f7dca5085cb3902261c3fa3ab9ede7696d641
Anuj <anuj.joshi@candylabs.de>
2019-03-31 21:12:56 +0200

    Add score

--- front-end/src/app/game/game.component.ts
+++ front-end/src/app/game/game.component.ts
@@ -1,37 +1,46 @@
-import { Component } from "@angular/core";
-import { environment } from "../../environments/environment";
+import { Component, Input, NgZone } from '@angular/core'
+import { environment } from '../../environments/environment'
 
-import createPhaser from "./createPhaser";
-import updatePhaser from "./updatePhaser";
-import preloadPhaser from "./preloadPhaser";
+import createPhaser from './createPhaser'
+import updatePhaser from './updatePhaser'
+import preloadPhaser from './preloadPhaser'
 
-import { SocketService } from "../socket/socket.service";
+import { SocketService } from '../socket/socket.service'
 
 let gameState = {
-  playerId: null
-};
+  playerId: null,
+}
 
-const sService = new SocketService();
+const sService = new SocketService()
 
 @Component({
-  selector: "game-component",
+  selector: 'game-component',
   template: `
-    <div class="game-wrap">
-      <phaser-component
-        [gameConfig]="gameConfig"
-        (gameReady)="onGameReady($event)"
-      ></phaser-component>
+    <div>
+      <div class="game-wrap">
+        <div>Score: {{ this.score }}</div>
+        <phaser-component
+          [gameConfig]="gameConfig"
+          (gameReady)="onGameReady($event)"
+        ></phaser-component>
+      </div>
     </div>
   `,
-  styleUrls: ["./game.component.scss"],
-  providers: [SocketService]
+  styleUrls: ['./game.component.scss'],
+  providers: [SocketService],
 })
 export class GameComponent {
   /**
    * Game instance.
    */
-  public game: Phaser.Game;
-  public values: any;
+
+  @Input()
+  playerName: string
+
+  score: string
+
+  public game: Phaser.Game
+  public values: any
 
   /**
    * Game configuration.
@@ -44,109 +53,113 @@ export class GameComponent {
     height: 416,
     scene: {
       create: function() {
-        createPhaser(this, gameState);
+        createPhaser(this, gameState)
       },
       preload: function() {
-        preloadPhaser(this);
+        preloadPhaser(this)
       },
       update: function() {
-        updatePhaser(this, gameState, sService);
+        updatePhaser(this, gameState, sService)
       },
-      world: {}
+      world: {},
     },
     physics: {
-      default: "arcade",
+      default: 'arcade',
       arcade: {
-        debug: false
-      }
-    }
-  };
+        debug: false,
+      },
+    },
+  }
 
   /**
    * Instantiate application component.
    */
-  constructor() {
-    sService.onConnectSuccess().subscribe(this.onConnectSuccess);
-    sService.onGameUpdate().subscribe(this.onGameUpdate);
-    sService.onPlayerDied().subscribe(this.onPlayerDied);
-    this.values = ":";
+  constructor(private ngZone: NgZone) {
+    sService.onConnectSuccess().subscribe(this.onConnectSuccess)
+    sService.onGameUpdate().subscribe(this.onGameUpdate)
+    sService.onPlayerDied().subscribe(this.onPlayerDied)
+    this.values = ':'
   }
 
   ngOnInit() {
-    let stringBuffer = "";
+    let stringBuffer = ''
+    console.log(this.playerName)
     document.onkeypress = evt => {
-      var charCode = evt.keyCode || evt.which;
-      var charStr = String.fromCharCode(charCode);
-      var shiftKeyTrue = evt.shiftKey;
+      var charCode = evt.keyCode || evt.which
+      var charStr = String.fromCharCode(charCode)
+      var shiftKeyTrue = evt.shiftKey
 
       if (stringBuffer.length > 20) {
-        stringBuffer = stringBuffer.slice(1, stringBuffer.length);
+        stringBuffer = stringBuffer.slice(1, stringBuffer.length)
       }
-      stringBuffer += charStr;
+      stringBuffer += charStr
 
-      const ggRegex = /^.*gg$/;
-      const ggCapsRegex = /^.*GG$/;
+      const ggRegex = /^.*gg$/
+      const ggCapsRegex = /^.*GG$/
 
       if (ggRegex.test(stringBuffer)) {
-        this.onBlinkToTop();
-        return;
+        this.onBlinkToTop()
+        return
       }
 
       if (ggCapsRegex.test(stringBuffer)) {
-        this.onBlinkToBottom();
-        return;
+        this.onBlinkToBottom()
+        return
       }
 
       if (/^.*bomb$/.test(stringBuffer)) {
-        this.addBomb();
-        return;
+        this.addBomb()
+        return
       }
 
       switch (charStr) {
-        case "h":
-          this.onChangePosition({ dx: -5, dy: 0, blink: false });
-          break;
-        case "l":
-          this.onChangePosition({ dx: 5, dy: 0, blink: false });
-          break;
-        case "j":
-          this.onChangePosition({ dy: 5, dx: 0, blink: false });
-          break;
-        case "k":
-          this.onChangePosition({ dy: -5, dx: 0, blink: false });
-          break;
-        case "e":
-          this.onChangePosition({ dx: -5, dy: 0, blink: true });
-          break;
-        case "b":
-          this.onChangePosition({ dx: 5, dy: 0, blink: true });
-          break;
+        case 'h':
+          this.onChangePosition({ dx: -5, dy: 0, blink: false })
+          break
+        case 'l':
+          this.onChangePosition({ dx: 5, dy: 0, blink: false })
+          break
+        case 'j':
+          this.onChangePosition({ dy: 5, dx: 0, blink: false })
+          break
+        case 'k':
+          this.onChangePosition({ dy: -5, dx: 0, blink: false })
+          break
+        case 'e':
+          this.onChangePosition({ dx: -5, dy: 0, blink: true })
+          break
+        case 'b':
+          this.onChangePosition({ dx: 5, dy: 0, blink: true })
+          break
       }
-    };
+    }
   }
 
   onConnectSuccess(data) {
-    gameState = data;
+    gameState = data
   }
 
-  onGameUpdate(data) {
-    gameState = data;
+  onGameUpdate = data => {
+    gameState = data
     // TODO @anuj move this scores to out on template
     let player = data.players[data.playerId]
-    console.log(player.score)
+    this.ngZone &&
+      this.ngZone.run(() => {
+        this.score = player.score
+      })
     // Object.values(data.players).forEach(player=>console.log(player.score))
   }
 
   onShoot() {
-    sService.shoot();
+    sService.shoot()
   }
 
   onPlayerDied() {
-    alert("YOU DEAD MAN");
+    alert('YOU DEAD MAN')
   }
 
   addBomb() {
-    sService.addBomb();
+    sService.addBomb()
   }
 
   /**
@@ -155,22 +168,22 @@ export class GameComponent {
    * @param game Game instance.
    */
   public onGameReady(game: Phaser.Game): void {
-    this.game = game;
+    this.game = game
   }
 
   onChangeDirection({ dx, dy }) {
-    sService.playerChangeDirection({ dx, dy, blink: false });
+    sService.playerChangeDirection({ dx, dy, blink: false })
   }
 
   onChangePosition({ dx, dy, blink }) {
-    sService.playerChangePosition({ dx, dy, blink });
+    sService.playerChangePosition({ dx, dy, blink })
   }
 
   onBlinkToTop() {
-    sService.onBlinkToTop();
+    sService.onBlinkToTop()
   }
 
   onBlinkToBottom() {
-    sService.onBlinkToBottom();
+    sService.onBlinkToBottom()
   }
 }
